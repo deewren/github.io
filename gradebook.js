0f9e088d398f330b1d2f688c60aba0a1aee5a312
deewren <dwren@my.waketech.edu>
2025-07-14 21:36:08 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+  console.log("Fetching grade data…");
+  return [];          // temporary placeholder data
+}
+
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
